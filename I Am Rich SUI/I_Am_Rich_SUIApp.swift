7b1b303f3ab16324aae2f8b8f1ef8e51ada5bdd4
Starchenkov <Starchenkov@live.ru>
2021-02-16 09:40:55 +0700

    Initial Commit

---
+++ I Am Rich SUI/I_Am_Rich_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_SUIApp.swift
+//  I Am Rich SUI
+//
+//  Created by Sergey Starchenkov on 09.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
